2b022099b2e5dd5215437f8c4577aab5243534f6
maulanaibrahim20 <maulibrahim20@student.polindra.ac.id>
2025-01-07 16:41:05 +0700

    feat : update

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,18 +17,19 @@ class BookFactory extends Factory
      */
     public function definition(): array
     {
+        $categoryIds = Category::pluck('id')->toArray();
         return [
-            'title' => $this->faker->sentence(3), // Judul buku acak
-            'author' => $this->faker->name, // Nama penulis acak
-            'publisher' => $this->faker->company, // Penerbit acak
-            'published_date' => $this->faker->date(), // Tanggal terbit acak
-            'isbn' => $this->faker->isbn13, // ISBN acak
-            'price' => $this->faker->randomFloat(2, 10000, 500000), // Harga antara 10 ribu hingga 500 ribu
-            'stock' => $this->faker->numberBetween(1, 100), // Stok antara 1-100
-            'description' => $this->faker->paragraph(3), // Deskripsi acak
-            'cover_image' => 'images/default_cover.jpg', // Atur default untuk testing
-            'status' => $this->faker->randomElement(['active', 'non-active']), // Status acak
-            'category_id' => null, // Isi dengan null atau sesuaikan dengan ID kategori
+            'title' => $this->faker->sentence(3),
+            'author' => $this->faker->name,
+            'publisher' => $this->faker->company,
+            'published_date' => $this->faker->date(),
+            'isbn' => $this->faker->isbn13,
+            'price' => $this->faker->randomFloat(2, 10000, 500000),
+            'stock' => $this->faker->numberBetween(1, 100),
+            'description' => $this->faker->paragraph(3),
+            'cover_image' => 'images/default_cover.jpg',
+            'status' => $this->faker->randomElement(['active', 'non-active']),
+            'category_id' => $this->faker->randomElement($categoryIds),
         ];
     }
 }
